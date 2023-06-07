d7959033fe28374154ceb8f4f0f15d56fd70027c
ECR_ <113091060+EuhCesAir@users.noreply.github.com>
2023-06-07 17:37:50 +0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,44 +1,116 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  ActivityIndicator,
+  FlatList,
+} from "react-native";
 import { BarCodeScanner } from "expo-barcode-scanner";
-import { useEffect, useState } from "react";
+import { SafeAreaView } from "react-native-web";
+
+/* const url = "http://127.0.0.1/html/komidi.json";
+
+const App = () => {
+  // Données en JSON
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(url)
+      .then((response) => response.json())
+      .then((json) => setData(json.representation))
+      .catch((error) => alert(error))
+      .finally(setLoading(false));
+  });
+
+  return (
+    <SafeAreaView style={styles.container}>
+      {isLoading ? (
+        <ActivityIndicator />
+      ) : (
+        <FlatList
+          data={data}
+          keyExtractor={({ id }, index) => id}
+          renderItem={({ item }) => {
+            <Text>
+              {item.jour} {item.heure}
+            </Text>;
+          }}
+        />
+      )}
+    </SafeAreaView>
+  );
+}; */
 
 export default function App() {
+  // Constantes Scanner 
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
-  const [text, setText] = useState("Pas encore scanné");
+  const [text, setText] = useState("Not yet scanned");
 
-  const askForPermission = () => {
+  const askForCameraPermission = () => {
     (async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === "granted");
     })();
   };
 
-  // Demander la permission pour utiliser la caméra
+  // Request Camera Permission
   useEffect(() => {
-    askForPermission();
+    askForCameraPermission();
   }, []);
 
-  // Ce qu'il se passe quand on scanne le QR Code
+  // What happens when we scan the bar code
   const handleBarCodeScanned = ({ type, data }) => {
     setScanned(true);
     setText(data);
     console.log("Type: " + type + "\nData: " + data);
   };
 
-  // Vérifier les permissions et renvoyer l'écran
+  // Check permissions and return the screens
   if (hasPermission === null) {
     return (
       <View style={styles.container}>
-        <Text>Demande d'autorisation de caméra</Text>
+        <Text>Requesting for camera permission</Text>
+      </View>
+    );
+  }
+  if (hasPermission === false) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ margin: 10 }}>No access to camera</Text>
+        <Button
+          title={"Allow Camera"}
+          onPress={() => askForCameraPermission()}
+        />
       </View>
     );
   }
 
+  // Return the View
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Komidi - Festival</Text>
+      <Text>Spectacle : </Text>
+      <Text>Date : </Text>
+      <Text>Salle : </Text>
+      <View style={styles.barcodebox}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={{ height: 400, width: 400 }}
+        />
+      </View>
+      <Text style={styles.maintext}>{text}</Text>
+
+      {scanned && (
+        <Button
+          title={"Scan again?"}
+          onPress={() => setScanned(false)}
+          color="tomato"
+        />
+      )}
     </View>
   );
 }
@@ -49,5 +121,18 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  maintext: {
+    fontSize: 16,
+    margin: 20,
+  },
+  barcodebox: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 300,
+    width: 300,
+    overflow: "hidden",
+    borderRadius: 30,
+    backgroundColor: "tomato",
   },
 });
